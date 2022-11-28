66e4af3258396c92e0ef7198bbe983e62b033a35
Adrian Aispuro <86083155+adrianaispuro@users.noreply.github.com>
2022-11-28 09:04:37 -0800

    added conditions to grade.sh

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,5 +1,7 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TestListExamples {
   @Test(timeout = 100)
@@ -7,4 +9,17 @@ public class TestListExamples {
     while(true) {}
   }
   // Write your grading tests here!
+  @Test
+  public void testFilter() {
+    List<String> input1 = new ArrayList<String>();
+    input1.add("Hello");
+    input1.add("Test");
+    input1.add("yes");
+
+    List<String> input2 = new ArrayList<String>();
+    input2.add("Hello");
+    input2.add("Test");
+    input2.add("yes");
+    assertEquals(input2, ListExamples.filter(input1, new StringChecker());
+  }
 }
--- grade.sh
+++ grade.sh
@@ -1,9 +1,45 @@
 # Create your grading script here
+CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
 
 rm -rf student-submission
 git clone $1 student-submission
+
 echo 'Finished cloning'
 
+cd student-submission
 
+if [[ -r "./ListExamples.java" ]]
+    then
+    echo "File found"
+    cp ./ListExamples.java ../
+    else
+    echo "Missing file"
+    echo "Grade: 0% - try again"
+    exit 1
+fi
+
+javac -cp $CPATH *.java 2> compile-err.txt
+
+if [[ $? -ne 0 ]]
+
+    then
+    echo "Compile error(s)!"
+    if [[ $(wc -l < compile-err.txt) -gt 1 ]]
+        then
+            echo "Grade: 10% - too many compile errors"
+        else
+            echo "Grade: 20% - one compile error"
+    fi
+    cat compile-err.txt
+    exit 1
+    else
+    echo "Compiled successfully"
+fi
+
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > test-results.txt 2> test-err.txt
+
+cat test-results.txt
+
+cat test-err.txt
 
 
